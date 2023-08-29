9a7616b0d3b05d8058ef43af2e7f304a24843412
Malebo16 <132347394+Malebo16@users.noreply.github.com>
2023-08-29 13:43:57 +0200

    Update ui.py

--- ui.py
+++ ui.py
@@ -37,30 +37,29 @@ def main():
     # User input for features
     st.header('Feature Input')
     
-    location_Type={0:'Urban',1: 'Rural'}
+    cellphone_access={0:'No',1: 'Yes'}
     def format_func(option):
         return location_Type[option]
-    feature1 = st.selectbox("Choose your Location Type", options=list(location_Type.keys()), format_func=format_func)
-
-    # cellphone_access={0:'No',1: 'Yes'}
-    # def format_func(option):
-    #     return cellphone_access[option]
-    # feature2 = st.selectbox("cellphone_access", options=list(cellphone_access.keys()), format_func=format_func)
+    feature1 = st.selectbox("cellphone_access", options=list(location_Type.keys()), format_func=format_func)
 
 
     feature2 = st.number_input('age_of_respondent', value=0)
 
+    gender_of_respondent={0:'Female',1: 'Male'}
+    def format_func(option):
+        return location_Type[option]
+    feature3 = st.selectbox("gender_of_respondent", options=list(location_Type.keys()), format_func=format_func)
 
     education_level={0:'Secondary education',1: 'Vocational/Specialised training',2: 'No formal education', 3: 'Primary education',4: 'Other/Dont know/RTA',5: 'Tertiary education'}
     def format_func(option):
         return education_level[option]
-    feature3 = st.selectbox("Choose your education_level", options=list(education_level.keys()), format_func=format_func)
+    feature4 = st.selectbox("Choose your education_level", options=list(education_level.keys()), format_func=format_func)
 
    
     Job_type={0:'Formally employed Government', 1: 'Formally employed Private',2: 'Remittance Dependent', 3: 'Self employed', 4: 'Informally employed',5: 'Farming and Fishing', 6: 'Government Dependent',7: 'Other Income',8: 'Dont Know/Refuse to answer', 9: 'No Income'}
     def format_func(option):
         return Job_type[option]
-    feature4 = st.selectbox("Choose your Job_type", options=list(Job_type.keys()), format_func=format_func)
+    feature5 = st.selectbox("Choose your Job_type", options=list(Job_type.keys()), format_func=format_func)
    
  
 
@@ -71,7 +70,7 @@ def main():
     # Perform predictions when the button is clicked
     if clicked:
         # Perform predictions using the selected model
-        prediction = model.predict([[feature1, feature2, feature3, feature4]])
+        prediction = model.predict([[feature1, feature2, feature3, feature4, feature5]])
         if prediction[0] == 0:
             pred="Yes"
         else:
