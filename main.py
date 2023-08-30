7b6173699b8891f7861c2caa0c045355b85c0fd3
Preshy05 <93598931+Preshy05@users.noreply.github.com>
2023-08-30 14:10:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,12 +29,13 @@ input_data = np.array([[feature1, feature2, feature3, feature4, feature5]])
 # Make predictions when a button is clicked
 if st.button("Predict"):
     try:
-    st.write("Input Data:", input_data)  # Log the input data
+        
+        st.write("Input Data:", input_data) 
 
     # Use the loaded model to make predictions
-    prediction = model.predict(input_data)
+        prediction = model.predict(input_data)
 
-    st.write("Raw Prediction:", prediction)  # Log the raw prediction
+        st.write("Raw Prediction:", prediction)  # Log the raw prediction
 
     # Display the prediction
     st.write(f"Bank account Approval Probability: {prediction[0, 0]}")
